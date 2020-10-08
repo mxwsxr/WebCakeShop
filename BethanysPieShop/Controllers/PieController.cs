174e7883c85f8f20c7f441582892ab123d0a9cc5
Albert <Albert@DESKTOP-923AJ44>
2020-10-09 01:35:59 +0200

    add controllers

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -6,10 +6,13 @@ using BethanysPieShop.Models;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
+        //these are local fields that are not initialsed. 
         private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
 
@@ -19,23 +22,56 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
+            IEnumerable<Pie> pies;
+            string currentCategory;
 
-            ViewBag.CurrentCategory = "Cheese cakes";
-            return View(piesListViewModel);
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
+
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    piesListViewModel.CurrentCategory = "Cheese cake";
+        //    //return the view model.
+        //    return View(piesListViewModel);
+        //}
 
         public IActionResult Details(int id)
         {
             var pie = _pieRepository.GetPieById(id);
-            if (pie == null)
+            if(pie == null)
             {
+                // return 404
                 return NotFound();
             }
             return View(pie);
+
         }
+
+        //// GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
     }
 }
