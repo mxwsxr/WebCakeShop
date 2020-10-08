4fc7dcd08576c28d5ff930c25a1d24f31739392b
Albert <Albert@DESKTOP-923AJ44>
2020-10-09 00:00:13 +0200

    add _layout and _viewstart

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
@@ -20,8 +21,11 @@ namespace BethanysPieShop.Controllers
 
         public ViewResult List()
         {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
             ViewBag.CurrentCategory = "Cheese cakes";
-            return View(_pieRepository.AllPies);
+            return View(piesListViewModel);
         }
     }
 }
