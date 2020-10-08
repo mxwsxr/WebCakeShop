91c80c20ecd06e3813561212d025b22c6e8fc784
Albert <Albert@DESKTOP-923AJ44>
2020-10-09 01:36:46 +0200

    adjust conn string

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,6 +6,7 @@ using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +16,10 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        //configuration coming from appsettings.json via an instance
         public IConfiguration Configuration { get; }
 
+        //constructor injection
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,13 +29,20 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllersWithViews();//services.AddMvc(); would also work still
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //registering services for dependancy injection.
             services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+            // add MVC method
+            // new in core 3.0
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,17 +52,29 @@ namespace BethanysPieShop
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            //use https
             app.UseHttpsRedirection();
+            //use ststaic files in wwwroot
             app.UseStaticFiles();
-
+            // add session middleware
+            app.UseSession();
+            //enable MVC routing
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                //responsible for mapping an incoming request to an action in a controller.
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{Id?}");
+                endpoints.MapRazorPages();
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
